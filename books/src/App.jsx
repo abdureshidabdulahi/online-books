b1c983b0a3ea8d37293a2912aafa1d2ae3a09da8
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-07-04 18:22:30 +0300

    update

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from "./components/header";
 import Body from './components/body';
+import Footer from './components/footer';
 function App(){
         return(
         <div className="container-all">
         <Header/>
         <Body />
+        <Footer />
         </div>
         );
 }
--- books/src/components/footer.jsx
+++ books/src/components/footer.jsx
@@ -3,7 +3,45 @@
 function Footer(){
     return(
         <div className="footer-container">
+            <div className="container">
+            <ul className="open-in-library">
+                <li>Vision</li>
+                 <li>Volunteer</li>
+                  <li>partner with us</li>
+                   <li>careers</li>
+                     <li>blog</li>
+                    <li>terms of services</li>
+                     <li>Donate</li>
+                     
 
+            </ul>
+            <ul className="discover">
+                <li>home</li>
+                 <li>books</li>
+                  <li>authors</li>
+                   <li>subjects</li>
+                    <li>collections</li>
+                     <li>advansed search</li>
+                      <li>return to top</li>
+
+            </ul>
+            <ul className="develop">
+                 <li>developer center</li>
+                  <li>api documentation</li>
+                   <li>bulk data dumbs</li>
+                    <li>writing bots</li>
+                     
+            </ul>
+            <ul className="help">
+                 <li>help center</li>
+                  <li>contact us</li>
+                   <li>suggesting edits</li>
+                    <li>add abook</li>
+                     <li>release notes</li>
+
+            </ul>
+            
+            </div>
         </div>
     );
 }
