59198247826436d6eb4d201bffb0fefd6c3ae99b
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-07-04 16:01:02 +0300

    update

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,8 +1,10 @@
-
-
+import './App.css'
+import Header from "./components/header";
 function App(){
         return(
-
+        <div className="container-all">
+        <Header/>
+        </div>
         );
 }
-export default App
+export default App;
---
+++ books/src/components/header.jsx
@@ -0,0 +1,33 @@
+import image1 from '../assets/kimberly-farmer-lUaaKCUANVI-unsplash.jpg'
+import './header.css'
+import SearchIcon from '@mui/icons-material/Search';
+function Header(){
+    return(
+        <div className="header-container">
+            <div className="searh-div"
+            style={{
+                backgroundImage: `url(${image1})`,
+                height:'400px',
+                width:'100%',
+                backgroundSize:'cover',
+                backgroundPosition:'center'
+
+            }}
+            >
+    <p className="library-intro">
+  Welcome to our Online Library! 📚 Search for books, authors,
+   or topics to start your reading journey.
+</p>
+<div className='input'>
+    <SearchIcon/>
+    <input type='text' placeholder='search your favorite book' />
+    <button>
+        <SearchIcon style={{fontSize:'30xp'}}/>
+    </button>
+</div>
+            </div>
+
+        </div>
+    );
+}
+export default Header;
