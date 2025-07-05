aeee2f3f9cc54e918331d849fce0e98d349b89da
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-07-05 16:20:17 +0300

    update

--- books/src/components/body.jsx
+++ books/src/components/body.jsx
@@ -5,22 +5,26 @@ import './body.css'
 function Body(){
     const [books,setbooks] =useState([])
     useEffect(()=>{
-        fetch('https://openlibrary.org/search.json?q=education&page=1')
+       
+         fetch('https://openlibrary.org/search.json?q=education&page=4')
         .then(response=>response.json())
         .then((data)=>{
             console.log(data.docs)
          setbooks(data.docs)
+       
     })
+    
        
     },[])
     return(
         <div className="body-container">
-            <h1>Latest Books</h1>
+           
             {books.map((book)=>(
                 <div className='books' 
                 style={{
                     backgroundImage:`url(https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg)`,
                     width:'150px',
+                    backgroundSize:'contain',
                     height:'200px'
                 }}
                 >
