40a1782511613f16a63b9129768e63ef039585f3
abdureshidabdulahi <abdureshidabdulahi@users.noreply.github.com>
2025-07-10 11:07:41 +0300

    update

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import Header from "./components/header";
 import Body from './components/body';
 import Footer from './components/footer';
+import { Route, Routes } from 'react-router-dom';
  
 function App(){
         return(
         <div className="container-all">
         <Header/>
         <Body />
-        <Footer />
+        <Footer/>
        
          
         </div>
